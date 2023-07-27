678a0f5b413d01c69e0b443080024d82b4dfd9c9
Ulysses Carlos <ulysses_carlos@protonmail.com>
2023-07-26 22:05:15 -0400

    setup.py: Changed Indentation

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ try:
 except ImportError:
     from distutils.core import setup
 
-config = {"description": "A small program to convert Japanese tags into English.",
-          "author": "Ulysses Carlos",
-          "url": "N/A",
-          "download_url": "https://github.com/ucarlos/Romanjize",
-          "author_email": "ucarlos1@student.gsu.edu",
-          "version": "0.15",
-          "install_requires": ['nose', 'mutagen', 'google_trans_new'],
-          "packages": ['Romanjize'],
-          "scripts": [],
-          "name": "romanjize"}
+config = {
+    "description": "A small program to convert Japanese tags into English.",
+    "author": "Ulysses Carlos",
+    "url": "N/A",
+    "download_url": "https://github.com/ucarlos/Romanjize",
+    "author_email": "ucarlos1@student.gsu.edu",
+    "version": "0.15",
+    "install_requires": ['nose', 'mutagen', 'google_trans_new'],
+    "packages": ['Romanjize'],
+    "scripts": [],
+    "name": "romanjize"
+}
 
 
 setup(**config)
